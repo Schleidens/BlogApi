a18b77a2b96ccd2211a0e0cec2c542b5be12dbac
Schleidens <alectineschleidens2016@gmail.com>
2023-06-05 15:52:40 -0400

    register new custom user model in settings

--- BlogApi/settings.py
+++ BlogApi/settings.py
@@ -103,6 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+#define the custom auth user model
+AUTH_USER_MODEL = 'authentication.User'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
