0a7734c80f33f80812d8013fd0e8653a330e7cff
Schleidens <alectineschleidens2016@gmail.com>
2023-06-05 15:52:07 -0400

    create a custom user model and make migrations

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
 # Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        if not email:
+            raise ValueError('The Email field must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self.create_user(email, password, **extra_fields)
+
+class User(AbstractUser):
+    bio = models.TextField(max_length=300, blank=True)
+    
+    objects = CustomUserManager()
