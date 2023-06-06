96f3a60e7659ca06f3f2784075c6c81e9f8556a6
Schleidens <alectineschleidens2016@gmail.com>
2023-06-06 14:56:03 -0400

    create view to handle the register new user endpoint request

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from knox.models import AuthToken
+
+from .serializers import registerNewUserSerializer
 
 # Create your views here.
+
+class registerNewUserView(APIView):
+    
+    def post(self, request):
+        serializer = registerNewUserSerializer(data=request.data)
+        
+        if serializer.is_valid(raise_exception=True):
+            #save the user instance from request
+            user = serializer.save()
+            
+            #create a new token with the user instance
+            token = AuthToken.objects.create(user)[1]
+            
+            data = {
+                'message': 'User created successfully',
+                'token': token
+            }
+            return Response(data)
+        
+        return Response(serializer.errors, status=400)
