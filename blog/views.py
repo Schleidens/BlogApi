baeb0cf5deb23a294ac2220c95e01a804cff9734
Schleidens <alectineschleidens2016@gmail.com>
2023-06-08 16:25:00 -0400

    create view for blog post base on modelViewset

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from rest_framework import permissions
+
+from knox.auth import TokenAuthentication
+
+
+from .serializers import blogSerializer
+from .models import blogPost
 
 # Create your views here.
+    
+    
+class blogViewset(ModelViewSet):
+    serializer_class = blogSerializer
+    queryset = blogPost.objects.all()
+    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+    
+
