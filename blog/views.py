63d20c9f2c0546e8191d667dddbb488c324c7a3b
Schleidens <alectineschleidens2016@gmail.com>
2023-06-09 13:56:59 -0400

    check for user in create, update, destroy methods and return error if set user is not the authenticated user

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework import permissions
+from rest_framework import permissions, serializers, status
+from rest_framework.response import Response
 
 from knox.auth import TokenAuthentication
 
@@ -17,4 +18,34 @@ class blogViewset(ModelViewSet):
     authentication_classes = [TokenAuthentication]
     permission_classes = [permissions.IsAuthenticated]
     
-
+    #override the save methods
+    def perform_create(self, serializer):
+        
+        #return error if a user try to send data as a non authenticated user
+        user = self.request.user
+        if serializer.validated_data['author'] != user:
+            raise serializers.ValidationError(
+                {"error": "User mismatch. The 'author' field must be the authenticated user."}
+                )
+        
+        serializer.save()
+        
+    #return error if non authenticated user try deleting data as other user
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response(
+                {"error": "You do not have permission to delete this post."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        return super().destroy(request, *args, **kwargs)
+    
+    #return error if non authenticated user try modifying data as other user
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        if instance.author != request.user:
+            return Response(
+                {"error": "You do not have permission to update this post."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        return super().update(request, *args, **kwargs)
