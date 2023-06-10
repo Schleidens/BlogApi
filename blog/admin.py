28f3137ac72614d638cdfb12c5cc7f7d73c73db8
Schleidens <alectineschleidens2016@gmail.com>
2023-06-10 15:53:46 -0400

    add comment in admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import blogPost
+from .models import blogPost, blogComment
 
 # Register your models here.
 
 admin.site.register(blogPost)
+admin.site.register(blogComment)
