163e503dfc9260e45489c90e0eeadd159992c993
Schleidens <alectineschleidens2016@gmail.com>
2023-06-12 15:20:29 -0400

    cors header settings and allow request from all origin in developement

--- BlogApi/settings.py
+++ BlogApi/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     
     'rest_framework',
     'knox',
+    'corsheaders',
     
     'authentication',
     'blog',
@@ -48,12 +49,15 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'BlogApi.urls'
 
