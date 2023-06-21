bc5f8dfb79ad6a2c9798161f3194a197e563de15
Schleidens <alectineschleidens2016@gmail.com>
2023-06-21 13:42:28 -0400

    create router url for user blogs

--- blog/urls.py
+++ blog/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import blogViewset, commentViewset
+from .views import blogViewset, commentViewset, UserBlogViewset
 
 router = routers.SimpleRouter()
 
 router.register(r'blogs', blogViewset)
 router.register(r'comments/(?P<slug>[-\w]+)', commentViewset)
+router.register(r'user-blogs', UserBlogViewset)
 
 urlpatterns = [
     path('', include(router.urls))
