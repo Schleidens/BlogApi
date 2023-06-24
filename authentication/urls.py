dec1a6b2297c9c77c3a9696673290f94bdbe2ac2
Schleidens <alectineschleidens2016@gmail.com>
2023-06-24 14:45:53 -0400

    add url for knox logoutView

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from .views import registerNewUserView, loginUserView
+from knox.views import LogoutView
 
 urlpatterns = [
     path('register/', registerNewUserView.as_view()),
-    path('login/', loginUserView.as_view())
+    path('login/', loginUserView.as_view()),
+    path('logout/', LogoutView.as_view())
 ]
